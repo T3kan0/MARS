ff8fbbcb4b898d6fbe77c937ef7a25769aad8438
Tekano Mbonani <142930601+T3kan0@users.noreply.github.com>
2023-09-20 23:59:27 +0200

    Update mars.py

--- MARSv1.01/mars.py
+++ MARSv1.01/mars.py
@@ -18,18 +18,18 @@ st.set_page_config(
 )
 z, astep, logo = st.columns([0.52, 0.38, 0.25])
 with z:
-    st.image('https://github.com/T3kan0/mars/blob/main/MARSv1.01/reg.png', width=250)
+    st.image('img/https://github.com/T3kan0/mars/blob/main/MARSv1.01/reg.png', width=250)
 with astep:
     st.markdown("<h1 style='text-align: left; color: darkred;'>A-STEP</h1>", unsafe_allow_html=True)
     st.markdown("<h6 style='text-align: left; color: grey;'>🧑🏼‍🎓 👨🏽‍🎓Supplemental</h6>", unsafe_allow_html=True)
     st.markdown("<h6 style='text-align: left; color: grey;'>Instruction (UFS)👨🏽‍🏫</h6>", unsafe_allow_html=True)    
 with logo:
-    st.image('https://github.com/T3kan0/mars/blob/main/MARSv1.01/reg.png', width=250)
+    st.image('img/https://github.com/T3kan0/mars/blob/main/MARSv1.01/reg.png', width=250)
 st.markdown("<h2 style='text-align: center; color: grey;'>Merging Attendance RegisterS (MARS)</h2>", unsafe_allow_html=True)
 st.write("<h4 style='text-align: center; color: grey;'>version: 1.0.</h4>", unsafe_allow_html=True)
 col1, col2 = st.columns([2,1], gap='small')
 with col1:
-    st.image('https://github.com/T3kan0/mars/blob/main/MARSv1.01/logo3.png')
+    st.image('img/https://github.com/T3kan0/mars/blob/main/MARSv1.01/logo3.png')
 with col2:
     st.write('Welcome to the A-STEP Web Application **MARS** v.1.0. This was developed for the merging of large weekly\
  attendance files for the A-STEP data intern team, as part of their weekly data pre-processing operations.')
@@ -39,7 +39,7 @@ with col2:
     captions = ["Default: Stacking Files Atop One Another 📕📗📘➡️📚",
                 "Combines Files Based on Columns in the Right File 👉🏾", "Combines Files Based on Columns in the Left File 👈🏾"])
 st.sidebar.markdown("<h1 style='text-align: center; color: grey;'>Centre for Teaching and Learning</h1>", unsafe_allow_html=True)
-st.sidebar.image('https://github.com/T3kan0/mars/blob/main/MARSv1.01/logio.jpeg')
+st.sidebar.image('img/https://github.com/T3kan0/mars/blob/main/MARSv1.01/logio.jpeg')
 
 bulk_files = st.sidebar.file_uploader('Upload Files',
                                      type=['xlsx', 'csv'],
@@ -77,7 +77,7 @@ if bulk_files is not None:
             col_n1 = bytes_data.columns
         with col4:
             st.write(':blue[Type : ]',uploaded_file.type)
-            st.image('https://github.com/T3kan0/mars/blob/main/MARSv1.01/tech.png', width=130)  
+            st.image('img/https://github.com/T3kan0/mars/blob/main/MARSv1.01/tech.png', width=130)  
     elif len(n_files) >=2:
         col5, col6 = st.columns([.90, 0.10], gap='small')
         with col3:
@@ -86,14 +86,14 @@ if bulk_files is not None:
             col_n1 = n_files[0].columns
         with col4:
             st.write(':blue[Type : ]',uploaded_file.type)
-            st.image('https://github.com/T3kan0/mars/blob/main/MARSv1.01/tech.png', width=120)
+            st.image('img/https://github.com/T3kan0/mars/blob/main/MARSv1.01/tech.png', width=120)
         with col5:
             st.write(':blue[Register ]',j+1)
             col5.write(n_files[1].head(2))
             col_n2 = n_files[1].columns
         with col6:
             st.write(':blue[Type : ]', uploaded_file.type)
-            st.image('https://github.com/T3kan0/mars/blob/main/MARSv1.01/tech.png', width=120)
+            st.image('img/https://github.com/T3kan0/mars/blob/main/MARSv1.01/tech.png', width=120)
 
               
     st.write(':blue[Number of Files Uploaded : ]', len(n_files))
